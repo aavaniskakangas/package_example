7aafd4c3f9349276b2f3ded65e1b3255887d0124
Aava Niskakangas <aava.niskakangas@student.hanken.fi>
2025-09-17 15:30:09 -0300

    chore: add gitignore

--- .venv/bin/hello.py
+++
@@ -1,6 +0,0 @@
-#!/Users/aavaniskakangas/Documents/projects/brasilia/package_example/.venv/bin/python3
-from dev_aberto import hello
-
-if __name__ == '__main__':
-    date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
